974d504a2b3d8ee69ab0b85a0a97835888d9012d
CODE WEB <gabrielronaldo9343@gmail.com>
2025-07-23 19:47:52 -0400

    add card

--- src/Components/Services/Services.tsx
+++ src/Components/Services/Services.tsx
@@ -1,10 +1,35 @@
-// import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
 import './Services.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faHelmetSafety,   
+  faUsers,          
+  faClipboardCheck,
+  faFile,           
+  faBookOpen,       
+  faStethoscope 
+} from '@fortawesome/free-solid-svg-icons';
 
 const Services = () => {
   return (
     <div className='services grid md:grid-cols-2 lg:grid-cols-3 gap-8'>
-       
+      
+       <div className="hover:shadow-lg transition-shadow border-l-4 border-l-blue-600">
+        <div>
+          <div className="flex items-center space-x-3">
+            <div className="p-2 bg-blue-100 rounded-lg">
+              <FontAwesomeIcon icon={faHelmetSafety} className="h-6 w-6 text-blue-600" />
+            </div>
+            <h2 className="text-xl">Treinamento NR-10</h2>
+          </div>
+        </div>
+        <div>
+          <p className="text-gray-600">
+            Treinamento completo sobre segurança em instalações e serviços em eletricidade, conforme norma
+            regulamentadora NR-10. Capacitação teórica e prática para prevenção de acidentes elétricos.
+          </p>
+        </div>
+        
+      </div>
     </div>
   )
 }
